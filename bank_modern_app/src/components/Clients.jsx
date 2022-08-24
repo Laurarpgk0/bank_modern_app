817cb7d28f2483ed9f0f2b23572a308acff02242
Laurarpgk0 <laurazt6@gmail.com>
2022-08-24 08:59:41 -0300

    finalizado

---
+++ bank_modern_app/src/components/Clients.jsx
@@ -0,0 +1,23 @@
+import { clients } from '../constants';
+import styles from '../style';
+
+const Clients = () => (
+  <section className={`${styles.flexCenter} mt-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={clients.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}
+        >
+          <img
+            src={client.logo}
+            alt="client"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
+
+export default Clients;
